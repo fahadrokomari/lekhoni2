7ab9745a9641db699eee043b17343e05680ad48a
ronniegnr <gnr.ronnie@gmail.com>
2017-07-16 18:00:05 +0600

    config server introduced

--- account-service/src/main/java/bd/com/ronnie/accountservice/web/rest/HelloController.java
+++ account-service/src/main/java/bd/com/ronnie/accountservice/web/rest/HelloController.java
@@ -1,12 +1,21 @@
 package bd.com.ronnie.accountservice.web.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController("")
+@RestController
 public class HelloController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return this.message;
+    }
 
     @GetMapping("hello")
     public String hello() {
